6b7a98d2f0128670008e59f3c745703d5ac38246
Alexander Mlakar <alexander.mlakar@learnet.se>
2018-01-15 12:17:23 +0100

    Kommentar om mig

--- LxChatt/Program.cs
+++ LxChatt/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace LxChatt
 {
+    //Av Alexander Mlakar NTI Gymnasiet 2018
+    //Utifrån aritkel från C-Sharpskolan http://csharpskolan.se/article/broadcast-och-chatt
     class Program
     {
         static void Main(string[] args)
