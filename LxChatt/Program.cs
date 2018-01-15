4b883666022978b957cca47077975ea49fcfc902
Alexander Mlakar <alexander.mlakar@learnet.se>
2018-01-15 13:09:30 +0100

    skapat listener metoden

--- LxChatt/Program.cs
+++ LxChatt/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,31 @@ namespace LxChatt
     //Utifrån aritkel från C-Sharpskolan http://csharpskolan.se/article/broadcast-och-chatt
     class Program
     {
+        private const int ListenPort = 11000;
         static void Main(string[] args)
         {
+        }
+
+        static void Listener()
+        {
+            UdpClient listener = new UdpClient(ListenPort);
+
+            try
+            {
+                IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                byte[] bytes = listener.Receive(ref groupEP);
+                Console.WriteLine("Recived broadcast from {0} : {1}\n", groupEP.ToString(), Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+            finally
+            {
+
+            }
         }
     }
 }
